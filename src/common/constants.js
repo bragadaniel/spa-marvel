5a89032a768206f37200326c9acdfa71128a480d
Daniel Braga <15815539+bragadaniel@users.noreply.github.com>
2025-01-14 22:22:56 -0300

    feat: update versions libs (#1)

---
+++ src/common/constants.js
@@ -0,0 +1,2 @@
+export const baseURL = process.env.REACT_APP_BASE_URL_API
+export const apikey = process.env.REACT_APP_PUBLIC_KEY
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,15 @@
 import "./styles.css";
 
-function Card({ src, titleImage, name, description, events }) {
+function Card({ ...props }) {
+  const { thumbnail: { extension, path }, name, description } = props
   return (
     <div className="containerCard">
       <div className="boxAvatar size1of4">
-        <img src={src} alt={titleImage} width="120" height="120" />
+        <img src={`${path}.${extension}`} alt={name} width="120" height="120" />
         <p className="nameCharacter">{name}</p>
       </div>
       <div className="size6of12">
         <p>{description}</p>
-      </div>
-      <div className="size1of4">
-        <p>{events}</p>
       </div>
     </div>
   );
